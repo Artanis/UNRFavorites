552f669c2ba878bd9165bfee4fc62d84673c5865
Erik Youngren <artanis.00@gmail.com>
2009-12-25 12:11:25 -0800

    More robust icon loading.

--- ui.py
+++ ui.py
@@ -4,10 +4,29 @@ UNRFavorites (C) 2009 Erik Youngren <artanis.00@gmail.com>
 import os
 import gtk
 import gtk.glade
+from glib import GError
 
 from SimpleGladeApp import SimpleGladeApp
 
 import GConfFavs
+
+DEFAULT_ICON = "gnome-fs-bookmark"
+WINDOW_ICON  = "gnome-fs-bookmark"
+
+def load_icon(path, size=64):
+    theme = gtk.icon_theme_get_default()
+    pixbuf = theme.load_icon(DEFAULT_ICON, size, gtk.ICON_LOOKUP_FORCE_SVG)
+    
+    try:
+        pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(path, size, size)
+    except GError:
+        try:
+            pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(
+                "/usr/share/app-install/icons/%s" % (path,) ,size, size)
+        except GError:
+            pixbuf = theme.load_icon(path, size, gtk.ICON_LOOKUP_FORCE_SVG)
+    
+    return pixbuf
 
 class FavoritesStore(gtk.ListStore):
     def __init__(self):
@@ -17,21 +36,7 @@ class FavoritesStore(gtk.ListStore):
         
         for fav in self.favlist.list_active():
             favorite = GConfFavs.Favorite(fav)
-            icon_path = favorite.icon_path
-            pixbuf = None
-            theme = gtk.icon_theme_get_default()
-            if icon_path is not None:
-                if icon_path[0] == "/":
-                    pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(icon_path, 64, 64)
-                else:
-                    try:
-                        pixbuf = theme.load_icon(icon_path, 64, gtk.ICON_LOOKUP_FORCE_SVG)
-                    except:
-                        pixbuf = theme.load_icon("gnome-fs-bookmark", 64, gtk.ICON_LOOKUP_FORCE_SVG)
-            else:
-                pixbuf = theme.load_icon("gnome-fs-bookmark", 64, gtk.ICON_LOOKUP_FORCE_SVG)
-            
-            self.append([pixbuf, favorite.name, favorite.entry_name])
+            self.append([load_icon(favorite.icon_path), favorite.name, favorite.entry_name])
 
 class FavoritesManager(SimpleGladeApp):
     def __init__(self):
@@ -43,6 +48,8 @@ class FavoritesManager(SimpleGladeApp):
         self.wxIconView.set_model(store)
         self.wxIconView.set_text_column(1)
         self.wxIconView.set_pixbuf_column(0)
+        
+        gtk.window_set_default_icon(load_icon(WINDOW_ICON))
     
     def on_wxToolbarBtnSave_clicked(self, button):
         model = self.wxIconView.get_model()
